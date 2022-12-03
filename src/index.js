b715db56b7a321b044c2c866b0df38048c549bc6
liepsnele <liepsnele.tech@gmail.com>
2022-12-04 00:03:40 +0200

    Props assignments

--- src/index.js
+++ src/index.js
@@ -1,68 +1,74 @@
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
 // Mini Book Project
-import ReactDom from "react-dom";
-import 
+
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/8144Vic9C5L._AC_UL300_SR300,200_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+};
+
+const secondBook = {
+  img: "https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg",
+  title: "Our Class is a Family",
+  author: "Shannon Olsen ",
+};
 
 function BookList() {
   return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+    <section className="booklist">
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque,
+          non reiciendis. Saepe eius odit hic aut, assumenda cum dolore
+          repellendus vel dolores repellat ea sed aliquid quisquam magnam eaque
+          odio?
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  const { img, title, author } = props;
+  console.log(props);
+
   return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
+    <article className="book">
+      <img src={img} alt="Book" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+      {props.children}
     </article>
   );
 };
 
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg"
-      alt="Book"
-    />
-  );
-};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<BookList />);
 
-const Title = () => {
-  return <h1>I Love You to the Moon and Back</h1>;
-};
+// First component
+// import ReactDom from "react-dom";
 
-const Author = () => {
-  return <h4>Amelia Hepworth</h4>;
-};
+// function Greeting() {
+//   return (
+//     <div>
+//       <Person />
+//       <Message />
+//     </div>
+//   );
+// }
 
-ReactDom.render(<BookList />, document.getElementById("root"));
+// const Person = () => <h2>Monika Levivckaitė</h2>;
+// const Message = () => <p>This is my message.</p>;
 
-/*First component
-import ReactDom from "react-dom";
-
-function Greeting() {
-  return (
-    <div>
-      <Person />
-      <Message />
-    </div>
-  );
-}
-
-const Person = () => <h2>Monika Levivckaitė</h2>;
-const Message = () => <p>This is my message.</p>;
-
-ReactDom.render(<Greeting />, document.getElementById("root"));*/
+// ReactDom.render(<Greeting />, document.getElementById("root"));
